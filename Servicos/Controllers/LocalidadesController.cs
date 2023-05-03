b9b12601d53a65c2c842b0eceb2c5c6f5d9fb84b
Etec <gustavo.silva1667@etec.sp.gov.br>
2023-05-03 19:55:25 -0300

    commit initial

---
+++ Servicos/Controllers/LocalidadesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+namespace Servicos.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LocalidadesController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> GetEstados()
+        {
+            List<String> list = new List<String>();
+            list.Add("SP");
+            list.Add("BA");
+            list.Add("MG");
+
+            return list;
+        }
+    }
+}
